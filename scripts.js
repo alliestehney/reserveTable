1d9d21a067b85c5c8a5e86ded52d3252e25af02b
alliestehney <alliestehney1@gmail.com>
2017-10-30 14:26:32 -0400

    Generates HTML content to say what table number is clicked on

--- scripts.js
+++ scripts.js
@@ -2,12 +2,20 @@ $(document).ready(function() {
  // all of the jQuery / JS goes in here 
  console.log("Hi");
 
-$(".circle").on("click", function(e) {
+$(".available").on("click", function(e) {
 	$("form").css("display", "block");
+	console.log($(this).text());
+	var tableNum = $(this).text();
+	$("p").text("Table Number: " + tableNum);
 });
 
 $("i").on("click", function(e) {
 	$("form").css("display", "none");
+});
+
+$("input[type=submit]").on("click", function(e) {
+	$(this).removeClass("available");
+	$(this).addClass("reserved");
 });
 
 });
