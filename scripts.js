2d3fdfb71502444e472f92614c74979bbc9f8823
alliestehney <alliestehney1@gmail.com>
2017-10-30 14:15:22 -0400

    Styled form to overlay other content

--- scripts.js
+++ scripts.js
@@ -2,5 +2,12 @@ $(document).ready(function() {
  // all of the jQuery / JS goes in here 
  console.log("Hi");
 
+$(".circle").on("click", function(e) {
+	$("form").css("display", "block");
+});
+
+$("i").on("click", function(e) {
+	$("form").css("display", "none");
+});
 
 });
