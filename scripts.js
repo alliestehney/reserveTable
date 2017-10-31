37b8fdc142dce0fff2e2df466cb5ca684a3f4866
alliestehney <alliestehney1@gmail.com>
2017-10-31 09:17:18 -0400

    Added if/else to prevent form from displaying when element has class reserved

--- scripts.js
+++ scripts.js
@@ -2,30 +2,28 @@ $(document).ready(function() {
  // all of the jQuery / JS goes in here 
 
 	// function to generate HTML content saying what table is clicked on
-	$(".available").on("click", function(e) {
-		$("form").css("display", "block");
-		var tableNum = $(this).text();
-		$("p#tableNum").text("Table Number: " + tableNum);
-		// console.log("***NO DOLLA***");
-		// console.log(this);
-		// console.log($(this));
+	$(".circle").on("click", function(e) {
+		console.log(this.className);
+		if (this.className === "circle available") {
+			$("form").css("display", "block");
+			var tableNum = $(this).text();
+			$("p#tableNum").text("Table Number: " + tableNum);
 
-		$("input[type=submit]").on("click", function(e) {
+			$("input[type=submit]").on("click", function(e) {
+				$("form").css("display", "none");
+				// $( "div:contains("+ tableNum +")").removeClass("available").addClass("reserved");
+				// e.target.parent;
+				e.preventDefault();
+				return $("div .circle:contains("+ tableNum.toString() + ")").removeClass("available").addClass("reserved");
+				
+			});
+		} else {
 			$("form").css("display", "none");
-			// $( "div:contains("+ tableNum +")").removeClass("available").addClass("reserved");
-			// e.target.parent;
-			e.preventDefault();
-			return $("div .circle:contains("+ tableNum.toString() + ")").removeClass("available").addClass("reserved");
-			
-		});
+		}
 
 	});
 
 	$("i").on("click", function(e) {
-		$("form").css("display", "none");
-	});
-
-	$(".circle reserved").on("click", function() {
 		$("form").css("display", "none");
 	});
 	
