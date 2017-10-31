4b10c13a4f10ed123d1f1157e25e3c3050c9e045
alliestehney <alliestehney1@gmail.com>
2017-10-30 22:21:23 -0400

    Cleaned up scripts file, added style changes for reserved class

--- scripts.js
+++ scripts.js
@@ -1,33 +1,32 @@
 $(document).ready(function() {
  // all of the jQuery / JS goes in here 
- console.log("Hi");
 
-// function to generate HTML content saying what table is clicked on
-$(".available").on("click", function(e) {
-	$("form").css("display", "block");
-	var tableNum = $(this).text();
-	$("p#tableNum").text("Table Number: " + tableNum);
-	// console.log("***NO DOLLA***");
-	// console.log(this);
-	// console.log($(this));
+	// function to generate HTML content saying what table is clicked on
+	$(".available").on("click", function(e) {
+		$("form").css("display", "block");
+		var tableNum = $(this).text();
+		$("p#tableNum").text("Table Number: " + tableNum);
+		// console.log("***NO DOLLA***");
+		// console.log(this);
+		// console.log($(this));
 
-	$("input[type=submit]").on("click", function(e) {
-		$("form").css("display", "none");
-		// $( "div:contains("+ tableNum +")").removeClass("available").addClass("reserved");
-		// e.target.parent;
-		e.preventDefault();
-		return $("div .circle:contains("+ tableNum.toString() + ")").removeClass("available").addClass("reserved");
+		$("input[type=submit]").on("click", function(e) {
+			$("form").css("display", "none");
+			// $( "div:contains("+ tableNum +")").removeClass("available").addClass("reserved");
+			// e.target.parent;
+			e.preventDefault();
+			return $("div .circle:contains("+ tableNum.toString() + ")").removeClass("available").addClass("reserved");
+			
+		});
+
 	});
 
-});
+	$("i").on("click", function(e) {
+		$("form").css("display", "none");
+	});
 
-$("i").on("click", function(e) {
-	$("form").css("display", "none");
-});
-
-// $("input[type=submit]").on("click", function(e) {
-// 	$(this).removeClass("available");
-// 	$(this).addClass("reserved");
-// });
-
+	$(".circle reserved").on("click", function() {
+		$("form").css("display", "none");
+	});
+	
 });
