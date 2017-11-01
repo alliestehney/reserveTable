8eed1a2b448de1e098a2ceb1117770b3587cb71f
alliestehney <alliestehney1@gmail.com>
2017-10-31 20:38:57 -0400

    Added functions to store input values

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
- // all of the jQuery / JS goes in here 
-
+ 
+ 	var reservedTables = [];
 	// function to generate HTML content saying what table is clicked on
 	$(".circle").on("click", function(e) {
 		console.log(this.className);
@@ -8,23 +8,61 @@ $(document).ready(function() {
 			$("form").css("display", "block");
 			var tableNum = $(this).text();
 			$("p#tableNum").text("Table Number: " + tableNum);
-
+			lastTableClicked = this;
+			console.log(tableNum);
+			
 			$("input[type=submit]").on("click", function(e) {
 				$("form").css("display", "none");
-				// $( "div:contains("+ tableNum +")").removeClass("available").addClass("reserved");
-				// e.target.parent;
 				e.preventDefault();
+
+				storeInfo();
+
+				function storeInfo() {
+					var nameVal = $("#name").val(); // gets name that is inputed
+					console.log(nameVal);
+
+					var seatVal = $("#seats").val(); // gets number of people
+					console.log(seatVal);
+
+					createObj(nameVal, seatVal);
+					addToArray(reservationInfo);
+					clearVals();
+				}
+
+				function createObj(nameValue, seatsValue) {
+					reservationInfo = {};
+					reservationInfo.name = nameValue;
+					reservationInfo.seats = seatsValue;
+					return reservationInfo;
+				}
+
+				function addToArray(reservationInfo) {
+					reservedTables.push(reservationInfo);
+					// console.log(reservedTables);
+					return reservedTables;
+				}
+				// store object in an array so you can access it later
+				clearVals();
 				return $("div .circle:contains("+ tableNum.toString() + ")").removeClass("available").addClass("reserved");
-				
 			});
+		
+			console.log(reservedTables); // logs the most updated list
+			console.log(reservedTables[0]);
 		} else {
 			$("form").css("display", "none");
 		}
+		// console.log(reservedTables);
 
 	});
 
 	$("i").on("click", function(e) {
 		$("form").css("display", "none");
 	});
-	
+
+	function clearVals() {
+		$("#name").val("");
+		$("#phone").val("");
+		$("#seats").val("");
+	}
+
 });
